b15358366807c24b79a6515557c62088fb43b226
Tait Hoyem <taitskywalker@gmail.com>
2016-05-29 19:01:51 -0600

    First Create.js start

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 window.onload = function(){
-    
+    stage = new createjs.Stage("main");
+    //Create a Shape DisplayObject.
+    circle = new createjs.Shape();
+    circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+    //Set position of Shape instance.
+    circle.x = circle.y = 50;
+    //Add Shape instance to stage display list.
+    stage.addChild(circle);
+    //Update stage will render next frame
+    stage.update();
 };
